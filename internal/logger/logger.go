d3fb62b06b7c523ea072f6ec0109c743830b48f9
Mateusz Dobek <mat.dobek@gmail.com>
2023-11-28 17:57:47 +0000

    logger now has ability to log different levels of messages (debug, info, warn, err, fatal)

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,11 +1,47 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Err(err error) {
-	fmt.Println("--- Error --- ", err)
+type level int
+
+const (
+	debug level = iota
+	info
+	warn
+	err // don't overshadow `error`
+	fatal
+)
+
+var levelNames = map[level]string{
+	debug: "DEBUG",
+	info:  "INFO",
+	warn:  "WARN",
+	err:   "ERROR",
+	fatal: "FATAL",
 }
 
-func Info(args ...string) {
-	fmt.Println("--- INFO --- ", args)
+func log(l level, msg string, args ...any) {
+	fmt.Printf("["+levelNames[l]+"] "+msg+"\n", args...)
+}
+
+func Debug(msg string, args ...any) {
+	log(debug, msg, args...)
+}
+
+func Info(msg string, args ...any) {
+	log(info, msg, args...)
+}
+
+func Warn(msg string, args ...any) {
+	log(warn, msg, args...)
+}
+
+func Error(e error) {
+	log(err, e.Error())
+}
+
+func Fatal(e error) {
+	log(fatal, e.Error())
 }
