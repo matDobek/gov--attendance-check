efd6381387bfe419bf0fcded47a93e5f3f0b7c23
Mateusz Dobek <mat.dobek@gmail.com>
2023-12-20 20:37:32 +0000

    Provide StatueStore

---
+++ backend/pkg/manager/statue_store/sql.go
@@ -0,0 +1,91 @@
+package statue_store
+
+import (
+	"github.com/matDobek/gov--attendance-check/internal/storage"
+	"github.com/matDobek/gov--attendance-check/pkg/manager"
+)
+
+//----------------------------------
+// New
+//----------------------------------
+
+func NewSQLStatueStore(s *storage.Storage) *SQLStatueStore {
+	return &SQLStatueStore{s: s}
+}
+
+//----------------------------------
+// StatueStore
+//----------------------------------
+
+type SQLStatueStore struct {
+	s *storage.Storage
+}
+
+var (
+	_ manager.CreateStatueStore = (*SQLStatueStore)(nil)
+	_ manager.AllStatuesStore   = (*SQLStatueStore)(nil)
+)
+
+func (s *SQLStatueStore) All() ([]manager.Statue, error) {
+	var result []manager.Statue
+
+	q := `
+		select id, title, term_number, session_number, voting_number from statues
+	`
+
+	stmt, err := s.s.PrimaryDB.Prepare(q)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		statue := manager.Statue{}
+
+		err := rows.Scan(
+			&statue.ID,
+			&statue.Title,
+			&statue.TermNumber,
+			&statue.SessionNumber,
+			&statue.VotingNumber)
+
+		if err != nil {
+			return nil, err
+		}
+
+		result = append(result, statue)
+	}
+
+	return result, nil
+}
+
+func (s *SQLStatueStore) Insert(statue manager.Statue) (manager.Statue, error) {
+	q := `
+    insert into statues (term_number, session_number, voting_number, title)
+      values ($1, $2, $3, $4)
+      returning id
+  `
+
+	stmt, err := s.s.PrimaryDB.Prepare(q)
+	if err != nil {
+		return manager.Statue{}, err
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRow(
+		statue.TermNumber,
+		statue.SessionNumber,
+		statue.VotingNumber,
+		statue.Title).Scan(&statue.ID)
+	if err != nil {
+		return manager.Statue{}, err
+	}
+
+	return statue, nil
+}
