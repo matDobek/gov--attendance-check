a1d54b4032aee9d3387df0c047de35b34d53ee62
Mateusz Dobek <mat.dobek@gmail.com>
2023-12-22 13:39:20 +0000

    Provide in memory store for statue

---
+++ backend/pkg/manager/statue_store/memory_test.go
@@ -0,0 +1,80 @@
+package statue_store
+
+import (
+	"testing"
+	"time"
+
+	"github.com/matDobek/gov--attendance-check/internal/testing/assert"
+	"github.com/matDobek/gov--attendance-check/internal/testing/logger"
+	"github.com/matDobek/gov--attendance-check/pkg/manager"
+)
+
+func TestMemStoreInsert(t *testing.T) {
+  t.Run("saves valid statue", func(t *testing.T) {
+    store := NewMemStore()
+
+    params := manager.NewStatueParams().
+      WithTermNumber(1).
+      WithSessionNumber(2).
+      WithVotingNumber(3).
+      WithTitle("foo")
+
+    time_before := time.Now()
+    result, err := store.Insert(*params)
+    time_after := time.Now()
+
+    assert.Error(t, err)
+    assert.Equal(t, result.TermNumber, 1)
+    assert.Equal(t, result.SessionNumber, 2)
+    assert.Equal(t, result.VotingNumber, 3)
+    assert.Equal(t, result.Title, "foo")
+
+    if !time_before.Before(result.CreatedAt) || !time_after.After(result.CreatedAt) {
+      logger.LogError(t, "created_at is not valid: %v", result.CreatedAt)
+    }
+
+    if !time_before.Before(result.UpdatedAt) || !time_after.After(result.UpdatedAt) {
+      logger.LogError(t, "updated_at is not valid: %v", result.UpdatedAt)
+    }
+  })
+}
+
+func TestMemStoreAll(t *testing.T) {
+  t.Run("returns empty slice when no rows", func(t *testing.T) {
+    store := NewMemStore()
+
+    result, err := store.All()
+
+    assert.Error(t, err)
+    assert.Equal(t, len(result), 0)
+  })
+
+  t.Run("returns all existing rows", func(t *testing.T) {
+    store := NewMemStore()
+
+    params1 := manager.NewStatueParams().
+      WithTermNumber(1).
+      WithSessionNumber(1).
+      WithVotingNumber(1).
+      WithTitle("foo")
+
+    params2 := manager.NewStatueParams().
+      WithTermNumber(1).
+      WithSessionNumber(1).
+      WithVotingNumber(2).
+      WithTitle("bar")
+
+    _, err := store.Insert(*params1)
+    assert.Error(t, err)
+
+    _, err = store.Insert(*params2)
+    assert.Error(t, err)
+
+    result, err := store.All()
+
+    assert.Error(t, err)
+    assert.Equal(t, len(result), 2)
+    assert.Equal(t, result[0].Title, "foo")
+    assert.Equal(t, result[1].Title, "bar")
+  })
+}
