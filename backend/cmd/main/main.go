3045266b72256965a7efe034ddcb4cb15301d7b4
Mateusz Dobek <mat.dobek@gmail.com>
2023-12-28 13:14:30 +0100

    Update

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -1,72 +1,27 @@
 package main
 
 import (
-	"errors"
-	"os"
-
-	"github.com/joho/godotenv"
-	"github.com/matDobek/gov--attendance-check/internal/logger"
+	"github.com/matDobek/gov--attendance-check/internal/server"
 	"github.com/matDobek/gov--attendance-check/internal/storage"
-	"github.com/matDobek/gov--attendance-check/pkg/manager"
+	"github.com/matDobek/gov--attendance-check/internal/utils"
+	"github.com/matDobek/gov--attendance-check/pkg/discovery"
+	"github.com/matDobek/gov--attendance-check/pkg/manager/politician_store"
 	"github.com/matDobek/gov--attendance-check/pkg/manager/statue_store"
+	"github.com/matDobek/gov--attendance-check/pkg/manager/vote_store"
 )
 
 func main() {
-	// dStatues, err := discovery.Run()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	storage := storage.NewStorage(utils.PrimaryDatabaseURL())
 
-	// dStatues := []discovery.Statue{
-	// 	{VotingNo: 1, SessionNo: 1, TermNo: 1, Title: "1st voting", Votes: []discovery.Vote{
-	// 		{Name: "Jan Kowalski", Party: "KO", Response: db.VoteResponseYes},
-	// 		{Name: "Adam Nowak", Party: "PiS", Response: db.VoteResponseNo},
-	// 		{Name: "Marek Zbirek", Party: "Trzecia Droga", Response: db.VoteResponseMaybe},
-	// 	}},
-	// 	{VotingNo: 1, SessionNo: 1, TermNo: 1, Title: "1st voting", Votes: []discovery.Vote{
-	// 		{Name: "Jan Kowalski", Party: "KO", Response: db.VoteResponseYes},
-	// 		{Name: "Adam Nowak", Party: "PiS", Response: db.VoteResponseNo},
-	// 		{Name: "Marek Zbirek", Party: "Trzecia Droga", Response: db.VoteResponseMaybe},
-	// 	}},
-	// }
-	//
-	err := godotenv.Load()
-	if err != nil {
-		logger.Fatal(errors.New("error loading .env file"))
-		return
-	}
+	statueStore     := statue_store.NewSQLStore(storage.PrimaryDB)
+	politicianStore := politician_store.NewSQLStore(storage.PrimaryDB)
+	voteStore       := vote_store.NewSQLStore(storage.PrimaryDB)
 
-	databaseURL := os.Getenv("DB__MAIN__URL")
-	if databaseURL == "" {
-		logger.Fatal(errors.New("no database url"))
-		return
-	}
+  err := discovery.DiscoverAndStore(statueStore, politicianStore, voteStore)
+  if err != nil {
+    panic(err)
+  }
 
-	storage := storage.NewStorage(databaseURL)
-	s := statue_store.NewSQLStatusStore(storage)
-	// v := manager.StatueParams{
-	// 	SessionNumber: 1,
-	// 	TermNumber:    1,
-	// 	VotingNumber:  1,
-	// 	Title:         "1st voting",
-	// }
-	//
-	// _, errs := manager.CreateStatue(s, v)
-	// if errs != nil {
-	// 	logger.Fatal(errs)
-	// }
-	//
-
-	statues, err := manager.AllStatues(s)
-	if err != nil {
-		logger.Fatal(err)
-	}
-	logger.Info("%#v", statues)
-
-	// db := db.NewDB(databaseURL)
-
-	// statues, votes, politicians := discovery.MapToDBFormat(dStatues)
-	// db := db.NewGovStore(statues, politicians, votes)
-	// server := server.NewGovServer(db)
-	// server.Start()
+	server := server.NewGovServer(statueStore, politicianStore, voteStore)
+	server.Start()
 }
