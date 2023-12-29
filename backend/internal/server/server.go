69a6261a38b3f152d73e60a66858b4a2ada68387
Mateusz Dobek <mat.dobek@gmail.com>
2023-12-29 14:56:31 +0100

    formatting

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -11,12 +11,13 @@ import (
 type GovServer struct {
 	router *http.ServeMux
 
-  statueStore manager.StatueStore
-  politicianStore manager.PoliticianStore
-  voteStore manager.VoteStore
+	statueStore     manager.StatueStore
+	politicianStore manager.PoliticianStore
+	voteStore       manager.VoteStore
 }
 
 func (s *GovServer) Start() {
+	logger.Info("Starting gov server on port 8080")
 	logger.Fatal(http.ListenAndServe(":8080", s.router))
 }
 
@@ -24,9 +25,9 @@ func NewGovServer(statueStore manager.StatueStore, politicianStore manager.Polit
 	s := &GovServer{
 		router: http.NewServeMux(),
 
-    statueStore: statueStore,
-    politicianStore: politicianStore,
-    voteStore: voteStore,
+		statueStore:     statueStore,
+		politicianStore: politicianStore,
+		voteStore:       voteStore,
 	}
 
 	s.router.Handle("/api/v1/statues/", http.HandlerFunc(s.handleStatues))
@@ -39,47 +40,47 @@ func NewGovServer(statueStore manager.StatueStore, politicianStore manager.Polit
 func (s *GovServer) handleStatues(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-  data, err := manager.AllStatues(s.statueStore)
-  if err != nil {
-    logger.Fatal(err)
-  }
+	data, err := manager.AllStatues(s.statueStore)
+	if err != nil {
+		logger.Fatal(err)
+	}
 
 	w.WriteHeader(http.StatusOK)
 
-  err = json.NewEncoder(w).Encode(data)
-  if err != nil {
-    logger.Fatal(err)
-  }
+	err = json.NewEncoder(w).Encode(data)
+	if err != nil {
+		logger.Fatal(err)
+	}
 }
 
 func (s *GovServer) handleVotes(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-  data, err := manager.AllVotes(s.voteStore)
-  if err != nil {
-    logger.Fatal(err)
-  }
+	data, err := manager.AllVotes(s.voteStore)
+	if err != nil {
+		logger.Fatal(err)
+	}
 
 	w.WriteHeader(http.StatusOK)
 
-  err = json.NewEncoder(w).Encode(data)
-  if err != nil {
-    logger.Fatal(err)
-  }
+	err = json.NewEncoder(w).Encode(data)
+	if err != nil {
+		logger.Fatal(err)
+	}
 }
 
 func (s *GovServer) handlePoliticians(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-  data, err := manager.AllPoliticians(s.politicianStore)
-  if err != nil {
-    logger.Fatal(err)
-  }
+	data, err := manager.AllPoliticians(s.politicianStore)
+	if err != nil {
+		logger.Fatal(err)
+	}
 
 	w.WriteHeader(http.StatusOK)
 
-  err = json.NewEncoder(w).Encode(data)
-  if err != nil {
-    logger.Fatal(err)
-  }
+	err = json.NewEncoder(w).Encode(data)
+	if err != nil {
+		logger.Fatal(err)
+	}
 }
